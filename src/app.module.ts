0228f7aadb9fd1fd27cb420bfdffc2cc90e9650c
siwat jermwatthana <siwat.j@hiveground.com>
2024-01-07 15:58:17 +0700

    [Feat]: Add Global decorator to DatabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ProductsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 
+@Global() // set this module as global so that it can be used in other modules without importing it in
 @Module({
   imports: [],
   controllers: [],
   providers: [DatabaseService],
+  exports: [DatabaseService], // for use DI in other modules
 })
 export class DatabaseModule {}
