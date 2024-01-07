34f406f607eea0121c225a72fad420e5110ba6d1
siwat jermwatthana <siwat.j@hiveground.com>
2024-01-07 15:31:54 +0700

    [Feat]: Add Docker Compose file and Prisma client dependency

---
+++ prisma/migrations/20240107082259_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `price` DOUBLE NOT NULL,
+    `sale` BOOLEAN NOT NULL DEFAULT false,
+    `avaliability` ENUM('IN_STOCK', 'ONLINE', 'OUT_OF_STOCK', 'COMING_SOON') NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    UNIQUE INDEX `Product_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
